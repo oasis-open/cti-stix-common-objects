c3c503397055dd1c26cb8fe9488305feedd1b703
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2021-03-31 20:45:24 -0400

    add more logic, identity, breakdown of steps in GH Action
    make validate_content.py query `git status` for any recent changes instead of checking the whole repository

--- scripts/validate_content.py
+++ scripts/validate_content.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
+import re
 import shlex
+import subprocess
 
 # external
 from stix2validator import ValidationError, codes, output, validate_string
@@ -10,8 +12,9 @@ from stix2validator.validator import FileValidationResults, ValidationOptions
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Validate STIX common objects')
-    parser.add_argument('repo_dir',
-                        help='directory with common objects content')
+    parser.add_argument('-repo_dir',
+                        help='directory with common objects content.'
+                             'If not provided it will attempt `git status` on current directory')
     parser.add_argument('-v', required=False, metavar='validator_options', default=None,
                         help='validator arguments')
     return parser.parse_args()
@@ -31,12 +34,32 @@ def get_validator_options(validator_args) -> ValidationOptions:
 def main():
     args = parse_args()
     return_value = 0
-    top_level_repo_directory = pathlib.Path(args.repo_dir).resolve()
+    top_level_repo_directory = pathlib.Path(args.repo_dir).resolve().rglob("*.json") if args.repo_dir else ""
+    modified_files = []
+    new_files = []
 
-    for json_filename in top_level_repo_directory.rglob("*.json"):
-        json_filename_str = str(json_filename)
+    if args.repo_dir:
+        sub_proc = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
+        if sub_proc.returncode != 0:
+            print("Error executing Git...")
+            print(sub_proc.stderr)
+            return -1
+
+        str_stdout = str(sub_proc.stdout)
+        modified_files = re.findall(r'modified:\s{3}(.*\.json)', str_stdout)
+        new_files = re.findall(r'new file:\s{3}(.*\.json)', str_stdout)
+
+    search_criteria = top_level_repo_directory or (modified_files + new_files)
+
+    for json_filename in search_criteria:
+        if not isinstance(json_filename, pathlib.Path):
+            json_filename_path = pathlib.Path(json_filename).resolve()
+        else:
+            json_filename_path = json_filename
+        json_filename_str = str(json_filename_path)
         try:
-            with json_filename.open('r', encoding='utf-8') as stix_file:
+            with json_filename_path.open('r', encoding='utf-8') as stix_file:
                 json_string = stix_file.read()
                 validation_results = validate_stix2_string(json_string, get_validator_options(args.v), json_filename_str)
                 if not validation_results.is_valid:
