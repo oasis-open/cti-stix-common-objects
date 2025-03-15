aa0e8d8bf463d0366a1ec820b7f91d12054e4b11
Rich Piazza <rpiazza@mitre.org>
2025-03-15 14:02:32 -0400

    add tests and update schemas and examples

---
+++ extension-definition-specifications/incident-ef7/test_examples.sh
@@ -0,0 +1,66 @@
+
+stix2_validator examples/example_2.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.2.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.1.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.2.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.3.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.4.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.5.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.6.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.3.2.7.1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/example_2.4.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+
+stix2_validator examples/incident_expo_1.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_expo_2.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_expo_3.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_expo_4.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+
+stix2_validator examples/incident_asset_context.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_duplication.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_indicators.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_observables.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_pii_report.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_ransom.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_sample_activity.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_sample_infrastructure.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator examples/incident_with_child.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+
+stix2_validator event/examples/no_pattern.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator event/examples/port_scanning.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+
+stix2_validator impact/examples/paid_ransom.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator impact/examples/pii_loss.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+stix2_validator impact/examples/superseded_impacts.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+
+stix2_validator task/examples/perform-patching.json -s /Users/rpiazza/git/stix/cti-stix-common-objects/extension-definition-specifications 
+echo "\n"
+
+
